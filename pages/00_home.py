af5af158593412b47b4a14d42cf5dbcfb903b198
Ching-mu CHEN <76989123+chingmu-kuroro@users.noreply.github.com>
2025-11-18 16:11:28 +0000

    modified:   pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -5,7 +5,7 @@ import solara
 def Page():
     
     solara.Markdown("### 水平排列 (Row)")
-    
+
     # 'with' 語法會自動將內部的所有元件放入 Row 容器
     with solara.Row():
         solara.Markdown("欄位 1", style={"width": "30%", "border": "1px solid red"})
@@ -20,6 +20,9 @@ def Page():
         solara.Markdown("這個元件在 Row 的下面")
         solara.Markdown("這個元件在更下面")
     
+    solara.Markdown("### 這是另一個置中的垂直排列容器 (Column)")
+
+    # 'with' 語法會自動將內部的所有元件放入 Column 容器
     with solara.Column(align="center"):
         markdown = """
         ## 3D Mapping with Leafmap and MapLibre
